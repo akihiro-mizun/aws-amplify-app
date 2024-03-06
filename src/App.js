551b5ff1915c50061e3b94796b6bc47d5479449b
akihiro-mizun <mizuno@gl-s.co.jp>
2024-03-06 16:29:15 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react"
 import './App.css';
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 
 function App() {
-  let history = useNavigate();
+  //let history = useNavigate();
 
   //2.使いたい箇所でpushメソッドにURLを指定して、使用する
-  function handleClick() {
-    history("/patternA/sample001");
-  }
+  //function handleClick() {
+  //  history("/patternA/sample001");
+  //}
 
-
+  //<button onClick={() => history('/patternA/sample001')}>
+  //Go home
+  //</button>
     
   return (
     <div className="App">
       <div>Hello World</div>
       <br />
-      <button onClick={() => history('/patternA/sample001')}>
-      Go home
-    </button>
+
     </div>
   );
 }
